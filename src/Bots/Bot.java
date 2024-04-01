abefe5d3aaa1610372d17051773e6be372ea9763
TeeMur <timurs.kzn@gmail.com>
2024-04-01 13:46:39 +0300

    бета-тест

--- src/Bots/Bot.java
+++ src/Bots/Bot.java
@@ -22,7 +22,7 @@ public class Bot {
 		String unitName, unitSpecName;
 		ArrayList<String> botUnitsNames = new ArrayList<>();
 		String[] specNameSplit;
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3 + difficulty; i++) {
 			unitNameCounter = 1;
 			choiceType = random.nextInt(unitsTyping.size());
 			choiceUnit = random.nextInt(unitsTyping.get(choiceType).size());
@@ -55,9 +55,6 @@ public class Bot {
 			));
 		}
 		if (botDifficulty == 5) {
-			botUnitsArray.add(new Chernomor(mapBasicFields));
-			botUnitsArray.add(new Chernomor(mapBasicFields));
-			botUnitsArray.add(new Chernomor(mapBasicFields));
 			botUnitsArray.add(new Chernomor(mapBasicFields));
 		}
 		doubleAttackersIndexList = new ArrayList<>();
